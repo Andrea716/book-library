728a5e9ecef6e25297d8b55c3129a0c0bd721d40
Andrea716 <andreavisagie07@gmail.com>
2024-10-08 18:16:47 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class', // You can change this to 'media' if preferred
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
