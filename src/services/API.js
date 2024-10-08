20cbdbb8a0928341f5cb44afba5b21eb97726e90
Andrea716 <andreavisagie07@gmail.com>
2024-10-08 18:29:23 +0200

    Create API.js

---
+++ src/services/API.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://openlibrary.org';
+
+export const searchBooks = async (query, type = 'title') => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/search.json`, {
+      params: {
+        [type]: query,
+      },
+    });
+    return response.data.docs;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getBookDetails = async (isbn) => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/api/books`, {
+      params: {
+        bibkeys: `ISBN:${isbn}`,
+        format: 'json',
+        jscmd: 'data',
+      },
+    });
+    return response.data[`ISBN:${isbn}`];
+  } catch (error) {
+    throw error;
+  }
+};
